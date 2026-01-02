60cc09e8ccc8eb061a1a6a274f3c1c7f1dcfcd52
John J. Gavel <john.gavel@gmail.com>
2026-01-02 16:48:17 -0500

    Auto-commit on 2026-01-02 16:48:17

--- GavelGPIO/library_used.h
+++
@@ -1,7 +0,0 @@
-/* Auto-generated library definitions */
-#ifndef GAVELGPIO_LIBRARIES_USED_H
-#define GAVELGPIO_LIBRARIES_USED_H
-
-#define TCA9555_GPIO
-
-#endif /* GAVELGPIOLIBRARIES_USED_H */
--- GavelLicense/library_used.h
+++
@@ -1,6 +0,0 @@
-/* Auto-generated library definitions */
-#ifndef GAVELLICENSE_LIBRARIES_USED_H
-#define GAVELLICENSE_LIBRARIES_USED_H
-
-
-#endif /* GAVELLICENSELIBRARIES_USED_H */
